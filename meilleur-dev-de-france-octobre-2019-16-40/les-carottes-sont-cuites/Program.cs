305feb596cd922a7680673af25efacc259ac54e5
yvzn <mail@ludeo.net>
2021-12-01 23:15:11 +0100

    Meilleur dev de france 2019 session 16:40

---
+++ meilleur-dev-de-france-octobre-2019-16-40/les-carottes-sont-cuites/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*******
+ * Read input from Console
+ * Use: Console.WriteLine to output your result to STDOUT.
+ * Use: Console.Error.WriteLine to output debugging information to STDERR;
+ *       
+ * ***/
+
+namespace CSharpContestProject
+{
+	class Program
+	{
+		static string message;
+		static string nbLignes;
+		static List<string> texte = new List<string>();
+
+		static void Main(string[] args)
+		{
+			string ligne;
+			while ((ligne = Console.ReadLine()) != null)
+			{
+				//
+				// Lisez les données et effectuez votre traitement */
+				//
+				if (message == null)
+				{
+					message = ligne;
+					continue;
+				}
+				if (nbLignes == null)
+				{
+					nbLignes = ligne;
+					continue;
+				}
+
+				texte.Add(ligne);
+			}
+
+			// Vous pouvez aussi effectuer votre traitement ici après avoir lu toutes les données 
+			int positionMessage = 0;
+			foreach (var ligneTexte in texte)
+			{
+				foreach (var caractere in ligneTexte)
+				{
+					if (positionMessage < message.Length && caractere == message[positionMessage])
+					{
+						positionMessage++;
+					}
+				}
+			}
+
+			var messageTrouve = positionMessage == message.Length;
+			Console.WriteLine(messageTrouve ? 1 : 0);
+		}
+	}
+}
---
+++ meilleur-dev-de-france-octobre-2019-16-40/transport-air/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+/*******
+ * Read input from Console
+ * Use: Console.WriteLine to output your result to STDOUT.
+ * Use: Console.Error.WriteLine to output debugging information to STDERR;
+ *       
+ * ***/
+
+namespace CSharpContestProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Participant moi = default;
+			Participant ami = default;
+
+			string ligne;
+			while ((ligne = Console.ReadLine()) != null)
+			{
+				//
+				// Lisez les données et effectuez votre traitement */
+				//
+				if (moi == null)
+				{
+					moi = Lire(ligne);
+					continue;
+				}
+
+				if (ami == null)
+				{
+					ami = Lire(ligne);
+					continue;
+				}
+			}
+
+			// Vous pouvez aussi effectuer votre traitement ici après avoir lu toutes les données 
+			moi.position = moi.gareDepart;
+			ami.position = ami.gareDepart;
+
+			while (moi.position != ami.position)
+			{
+				while (moi.position < ami.position)
+				{
+					moi.position = (moi.position + moi.ligneEmpruntee) % 37;
+				}
+				while (ami.position < moi.position)
+				{
+					ami.position = (ami.position + ami.ligneEmpruntee) % 37;
+				}
+			}
+
+			Console.WriteLine(moi.position);
+		}
+
+		private static Participant Lire(string ligne)
+		{
+			var split = ligne.Split(' ');
+			return new Participant
+			{
+				gareDepart = int.Parse(split[0]),
+				ligneEmpruntee = int.Parse(split[1]),
+			};
+		}
+	}
+
+	class Participant
+	{
+		public int gareDepart;
+		public int ligneEmpruntee;
+		public int position;
+	}
+}
