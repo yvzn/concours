619a27df14a9791be8f19aa2ceb70c916684734d
yvzn <mail@ludeo.net>
2023-03-09 16:07:45 +0100

    Master Dev France 2023 session 15:30

--- master-dev-france-2023/exercice-1/Program.cs
+++ master-dev-france-2023/exercice-1/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace exercice_1
+/*******
+ * Read input from Console
+ * Use: Console.WriteLine to output your result to STDOUT.
+ * Use: Console.Error.WriteLine to output debugging information to STDERR;
+ *       
+ * ***/
+
+namespace CSharpContestProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string line;
+			int X = 0, Y = 0, N = 0;
+			while ((line = Console.ReadLine()) != null)
+			{
+				//
+				// Lisez les données et effectuez votre traitement */
+				//
+				var ligne = Lire1(line);
+				X = ligne.Item1;
+				Y = ligne.Item2;
+				N = ligne.Item3;
+			}
+
+			// Vous pouvez aussi effectuer votre traitement ici après avoir lu toutes les données
+			Console.WriteLine(X * Y % N == 0 ? "YES" : "NO");
+		}
+
+		private static (int, int, int) Lire1(string ligne)
+		{
+			var split = ligne.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+			return (split[0], split[1], split[2]);
+		}
+	}
 }
--- master-dev-france-2023/exercice-3/Program.cs
+++ master-dev-france-2023/exercice-3/Program.cs
@@ -1,12 +1,132 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace exercice_3
+/*******
+ * Read input from Console
+ * Use: Console.WriteLine to output your result to STDOUT.
+ * Use: Console.Error.WriteLine to output debugging information to STDERR;
+ *       
+ * ***/
+
+namespace CSharpContestProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static int? N = default;
+		static List<List<string>> ingredients = new List<List<string>>();
+		static HashSet<string> tousLesIngredients = new HashSet<string>();
+		static void Main(string[] args)
+		{
+			string line;
+			while ((line = Console.ReadLine()) != null)
+			{
+				//
+				// Lisez les données et effectuez votre traitement */
+				//
+				if (!N.HasValue)
+				{
+					N = int.Parse(line);
+					continue;
+				}
+
+				var ligne = Lire2(line).ToList();
+				ingredients.Add(ligne);
+				ligne.ForEach(i => tousLesIngredients.Add(i));
+			}
+
+			// Vous pouvez aussi effectuer votre traitement ici après avoir lu toutes les données
+			for (var nb = 1; nb < tousLesIngredients.Count && nb < 13; ++nb)
+			{
+
+				foreach (var combinaison in GenererCombinaison(nb))
+				{
+					if (TousLesClientsSatisfaits(combinaison))
+					{
+						Console.WriteLine(nb);
+						return;
+					}
+				}
+			}
+		}
+
+		private static bool TousLesClientsSatisfaits(IEnumerable<string> combinaison)
+		{
+			var list = combinaison.ToList();
+			foreach (var client in ingredients)
+			{
+				if (!ClientSatisfait(client, list))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private static bool ClientSatisfait(List<string> client, IList<string> combinaison)
+		{
+			return client.Intersect(combinaison).Any();
+		}
+
+		private static IEnumerable<IEnumerable<string>> GenererCombinaison(int nb)
+		{
+			return Combinations(tousLesIngredients, nb);
+		}
+
+		private static bool NextCombination(IList<int> num, int n, int k)
+		{
+			bool finished;
+
+			var changed = finished = false;
+
+			if (k <= 0) return false;
+
+			for (var i = k - 1; !finished && !changed; i--)
+			{
+				if (num[i] < n - 1 - (k - 1) + i)
+				{
+					num[i]++;
+
+					if (i < k - 1)
+						for (var j = i + 1; j < k; j++)
+							num[j] = num[j - 1] + 1;
+					changed = true;
+				}
+				finished = i == 0;
+			}
+
+			return changed;
+		}
+
+		private static IEnumerable<IEnumerable<T>> Combinations<T>(IEnumerable<T> elements, int k)
+		{
+			var elem = elements.ToArray();
+			var size = elem.Length;
+
+			if (k > size) yield break;
+
+			var numbers = new int[k];
+
+			for (var i = 0; i < k; i++)
+				numbers[i] = i;
+
+			do
+			{
+				yield return numbers.Select(n => elem[n]);
+			} while (NextCombination(numbers, size, k));
+		}
+
+		private static (int, int) Lire1(string ligne)
+		{
+			var split = ligne.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+			return (split[0], split[1]);
+		}
+
+		private static IEnumerable<string> Lire2(string ligne)
+		{
+			var split = ligne.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+			return split;
+		}
+	}
 }
